948ee7cd13eb413eaed028f3bdffb6365219296f
HareeshaBugga <31989819+HareeshaBugga@users.noreply.github.com>
2017-10-20 15:22:02 -0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,10 +1,26 @@
-# Lookup via location name.
-location = weather.lookup_by_location('halifax')
-condition = location.condition()
+import datetime
+import json
+import urllib.request
 
-# Get weather forecasts for the upcoming days.
-for forecasts in location.forecast():
-    print (forecasts['text'])
-    print (forecasts['date'])
-    print (forecasts['high'])
-    print (forecasts['low'])
+
+def time_converter(time):
+    converted_time = datetime.datetime.fromtimestamp(
+        int(time)
+    ).strftime('%I:%M %p')
+    return converted_time
+
+
+def url_builder(city_id):
+    user_api = 'http://openweathermap.org/city/6324729' 
+    unit = 'metric'  
+    api = 'http://api.openweathermap.org/data/2.5/weather?id=6324729'    
+
+    full_api_url = api + str(city_id) + '&mode=json&units=' + unit + '&APPID=' + user_api
+    return full_api_url
+
+def data_fetch(full_api_url):
+    url = urllib.request.urlopen(full_api_url)
+    output = url.read().decode('utf-8')
+    raw_api_dict = json.loads(output)
+    url.close()
+    return raw_api_dict
