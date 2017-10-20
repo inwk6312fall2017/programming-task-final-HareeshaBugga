77797accf5e596aff423d74995cfbd03295b6c99
HareeshaBugga <HareeshaBugga@dal.ca>
2017-10-20 14:59:23 -0300

    task1

--- task1.py
+++ task1.py
@@ -1,1 +1,13 @@
-x
+import string
+import csv
+with open('Book1.txt','r') as a, open('Book2.txt','r') as b, open('Book3.txt', 'r') as c :
+	longest=0
+	for line in a,b,c:
+		print (line)
+		for word in line:
+			print (word)
+			if len(word) > longest:
+				longest=len(word)
+				longest_word = word
+				print (longest_word)
+
