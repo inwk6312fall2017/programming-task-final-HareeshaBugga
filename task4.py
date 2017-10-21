60b3dd23a48ddba4a831900bb8a708fa947cb071
HareeshaBugga <31989819+HareeshaBugga@users.noreply.github.com>
2017-10-21 13:40:35 -0300

    Create task4.py

---
+++ task4.py
@@ -0,0 +1,15 @@
+
+import create_ticket as ticketgen
+import requests
+
+def getnetworkdevicecount():
+    ticket = ticketgen.get_ticket()
+    api = "/api/v1/network-device/count"
+    controller = 'sandboxapic.cisco.com'
+    url = "https://" + controller + api
+    header_content = {"content-type": "application/json", "X-Auth-Token": ticket}
+    response_val = requests.get(url, headers=header_content, verify=False)
+    response_json = response_val.json()
+    print("Number of hosts: ", response_json["response"])
+
+getnetworkdevicecount()
